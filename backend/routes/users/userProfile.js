450d498d976425e38f3431f083eae78f8bf7d6aa
rakshitha-sathyakumar <rakshu595@gmail.com>
2021-04-22 00:42:15 -0700

    Add backend routes to user profile

---
+++ backend/routes/users/userProfile.js
@@ -0,0 +1,42 @@
+const { Router } = require('express');
+const express = require('express');
+const mysql = require('../../config/sqlConnection');
+
+const router = express.Router();
+
+
+router.get('/getDetails/:email', (req, res) => {
+    const sql = `SELECT * FROM Users WHERE email = '${req.params.email}'`
+    
+    mysql.query(sql, (err, result) => {
+    console.log(result[0][0])
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Some error has occured');
+      }
+      if (result && result.length > 0 && result[0]) {
+        console.log(result)
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(JSON.stringify(result[0]));
+      }
+    });
+  });
+
+  router.post('/updateDetails/:email', (req, res) => {
+    const sql = `UPDATE Users 
+    SET firstName = '${req.body.firstName}', lastName = '${req.body.lastName}', phone = '${req.body.phone}', headline = '${req.body.headline}'
+    WHERE email = '${req.params.email}'`
+    mysql.query(sql, (err, result) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Some error has occured');
+      }
+      if (result && result.affectedRows > 0) {
+        console.log("Hiiiiii")
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('USER_UPDATED');
+      }
+    });
+  });
+
+module.exports = router;
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,53 +1,60 @@
-import React, { Fragment } from 'react';
+import React, { Component } from 'react';
 import logo from './images/logo.jpg';
 import 'bootstrap/dist/js/bootstrap.bundle';
 
-const Navbar = () => {
-  return (
-    <Fragment>
-      <nav class='navbar navbar-expand-lg navbar-dark bg-dark'>
-        <a class='navbar-brand' href='/home'>
-          <img src={logo} width='30' height='30' alt='' /> Trip It
-        </a>
+class Navbar extends Component {
+  handleLogout = () => {
+    window.localStorage.clear();
+    window.location ='/'
+  };
 
-        <div class='collapse navbar-collapse' id='navbarSupportedContent'>
-          <ul class='navbar-nav mr-auto'>
-            <li class='nav-item active'>
-              <a class='nav-link' href='/home'>
-                Home <span class='sr-only'>(current)</span>
-              </a>
-            </li>
-            <li class='nav-item'>
-              <a class='nav-link' href='/gettingStarted'>
-                Get Started
-              </a>
-            </li>
-          </ul>
-        </div>
+  render() {
+    return (
+        <div>
+          <nav class='navbar navbar-expand-lg navbar-dark bg-dark'>
+            <a class='navbar-brand' href='/home'>
+              <img src={logo} width='30' height='30' alt='' /> Trip It
+            </a>
 
-        {localStorage.token && (
-          <div class='nav-item dropdown mr-3'>
-            <button
-              class='btn btn-secondary dropdown-toggle'
-              type='button'
-              id='dropdownMenuButton'
-              data-toggle='dropdown'
-            >
-              <i class='far fa-user'></i>
-            </button>
-            <div class='dropdown-menu dropdown-menu-right'>
-              <a class='dropdown-item' href='/profile'>
-                Profile
-              </a>
-              <a class='dropdown-item' href='/'>
-                Logout
-              </a>
+            <div class='collapse navbar-collapse' id='navbarSupportedContent'>
+              <ul class='navbar-nav mr-auto'>
+                <li class='nav-item active'>
+                  <a class='nav-link' href='/home'>
+                    Home <span class='sr-only'>(current)</span>
+                  </a>
+                </li>
+                <li class='nav-item'>
+                  <a class='nav-link' href='/gettingStarted'>
+                    Get Started
+                  </a>
+                </li>
+              </ul>
             </div>
-          </div>
-        )}
-      </nav>
-    </Fragment>
-  );
+
+            {localStorage.token && (
+              <div class='nav-item dropdown mr-3'>
+                <button
+                  class='btn btn-secondary dropdown-toggle'
+                  type='button'
+                  id='dropdownMenuButton'
+                  data-toggle='dropdown'
+                >
+                  <i class='far fa-user'></i>
+                </button>
+                <div class='dropdown-menu dropdown-menu-right'>
+                  <a class='dropdown-item' href='/userProfile'>
+                    Profile
+                  </a>
+                  <a class='dropdown-item' href="#" onClick={this.handleLogout}>
+                    Logout
+                  </a>
+                </div>
+              </div>
+            )}
+          </nav>
+        </div>
+      );
+    }
 };
 
 export default Navbar;
